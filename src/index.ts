5c25b608e049f9b1d8cb52b1b4e064e8cec9d8c9
moumc <zmy9122@163.com>
2021-02-22 11:57:22 +0800

    modify to aitd version

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as brorand from 'brorand'
 import * as hashjs from 'hash.js'
 import * as elliptic from 'elliptic'
 
-import * as addressCodec from 'ripple-address-codec'
+import * as addressCodec from 'aitd-address-codec'
 import { derivePrivateKey, accountPublicFromPublicGenerator } from './secp256k1'
 import * as utils from './utils'
 
@@ -26,11 +26,7 @@ function generateSeed(
 }
 
 function hash(message): number[] {
-  return hashjs
-    .sha512()
-    .update(message)
-    .digest()
-    .slice(0, 32)
+  return hashjs.sha512().update(message).digest().slice(0, 32)
 }
 
 const secp256k1 = {
@@ -44,10 +40,7 @@ const secp256k1 = {
     const prefix = '00'
 
     const privateKey =
-      prefix +
-      derivePrivateKey(entropy, options)
-        .toString(16, 64)
-        .toUpperCase()
+      prefix + derivePrivateKey(entropy, options).toString(16, 64).toUpperCase()
 
     const publicKey = bytesToHex(
       Secp256k1.keyFromPrivate(privateKey.slice(2))
